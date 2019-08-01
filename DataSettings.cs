51e6e7aa38773ff14ac378ae9a2c8c97e4751ab4
Boris <borismor@ngs.ru>
2019-08-01 22:51:57 +0700

    first

---
+++ DataSettings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Shell
+{
+    class DataSettings
+    {
+        protected static DataSettings settings;
+        public List<ItemSettings> items { get; set; }
+
+        /// <summary>
+        /// Получение объета с данными
+        /// </summary>
+        /// <returns></returns>
+        public static DataSettings GetData()
+        {
+            if (DataSettings.settings != null)
+            {
+                return DataSettings.settings;
+            }
+
+            DataSettings.settings = new DataSettings();
+            return DataSettings.settings;
+        }
+
+        private void FakeData()
+        {
+            ItemSettings setting = new ItemSettings();
+            setting.text = "Тестовая кнопка";
+            this.items.Add(setting);
+        }
+
+        /// <summary>
+        /// Загражаем параметры
+        /// </summary>
+        public void LoadData()
+        {
+            this.FakeData();
+        }
+
+        public DataSettings()
+        {
+            this.LoadData();
+        }
+    }
+
+    public class ItemSettings
+    {      
+        public string text { get; set; }
+        public string exeFile { get; set; }
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Shell
+{
+    public partial class frmMain : Form
+    {
+     
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            DataSettings data = DataSettings.GetData();
+            for (int i = 0; i < data.items.Count; i++)
+            {
+                this.createButton(data.items[i]);
+            }
+        }
+
+        private void createButton(ItemSettings item)
+        {
+            Button button = new Button();
+            button.Text = item.text;
+            button.Parent = this;
+        }
+    }
+}
